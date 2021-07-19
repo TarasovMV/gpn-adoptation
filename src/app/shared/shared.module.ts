b71c63b873bd26204c005dce68d76f610f947547
Kirill Kim <kirill.kim@hotmail.com>
2021-07-19 12:02:39 +0300

    fix BrowserMod and colors

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -20,7 +20,6 @@ import { BrowserModule } from '@angular/platform-browser';
     ],
     imports: [
         CommonModule,
-        BrowserModule,
         IonicModule,
         AngularSvgIconModule,
     ],
