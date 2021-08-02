2f3a39abcc2992158c350e43e9ab0570b935d4e1
Tarasov Maxim <tarasovm2013@gmail.com>
2021-08-02 17:20:43 +0300

    add prompt

--- src/app/shared/components/info-popup/info-popup.component.ts
+++ src/app/shared/components/info-popup/info-popup.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {ModalController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-info-popup',
-  templateUrl: './info-popup.component.html',
-  styleUrls: ['./info-popup.component.scss'],
+    selector: 'app-info-popup',
+    templateUrl: './info-popup.component.html',
+    styleUrls: ['./info-popup.component.scss'],
 })
 export class InfoPopupComponent implements OnInit {
 
-  public blocks = [
-    'Информационный блок', 'Блок с заданием', 'Завершение этапа'
-  ];
+    public readonly blocks: string[] = [
+        'Информационный блок', 'Блок с заданием', 'Завершение этапа'
+    ];
 
-  constructor(
-    public modalController: ModalController
-  ) { }
+    constructor(
+        private modalController: ModalController
+    ) {}
 
-  ngOnInit() { }
+    public ngOnInit(): void {}
 
-  public dismiss() {
-    this.modalController.dismiss();
-  }
+    public dismiss() {
+        this.modalController.dismiss().then();
+    }
 
 }
