cfdac4d90f1685f6f6c49cb78939cf2e3194c7ec
Tarasov Maxim <tarasovm2013@gmail.com>
2021-08-03 00:18:01 +0300

    fix black areas

--- src/app/pages/tabs/pages/tabs-about/components/tabs-about-person/tabs-about-person.component.ts
+++ src/app/pages/tabs/pages/tabs-about/components/tabs-about-person/tabs-about-person.component.ts
@@ -1,28 +1,25 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
 import {TabsService} from 'src/app/core/services/tabs/tabs.service';
 import {IBullet} from '../../tabs-about.page';
 import {NavController} from "@ionic/angular";
+import {IPageTab, PageTabType} from "../../../../tabs.model";
 
 @Component({
     selector: 'app-tabs-about-person',
     templateUrl: './tabs-about-person.component.html',
     styleUrls: ['./tabs-about-person.component.scss'],
 })
-export class TabsAboutPersonComponent implements OnInit {
-
-    public id: string;
+export class TabsAboutPersonComponent implements OnInit, IPageTab {
+    public route: PageTabType = 'about';
     public data: IBullet[];
 
     constructor(
-        private route: ActivatedRoute,
-        public navCtrl: NavController,
-        public tabsService: TabsService
+        public tabsService: TabsService,
+        private navCtrl: NavController,
     ) {
     }
 
     ngOnInit() {
-        this.id = this.route.snapshot.paramMap.get('id');
         this.tabsService.person$.subscribe(value => {
             this.data = value.bullets;
         });
