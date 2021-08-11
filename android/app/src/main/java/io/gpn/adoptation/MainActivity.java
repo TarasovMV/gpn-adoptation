a85f55cd3b8875fbafb0b6a8697a4282586145c3
Mikhail Gunenkov <m.gunenkov@gmail.com>
2021-08-11 17:36:41 +0600

    Reset badge counter

--- android/app/src/main/java/io/gpn/adoptation/MainActivity.java
+++ android/app/src/main/java/io/gpn/adoptation/MainActivity.java
@@ -1,5 +1,22 @@
 package io.gpn.adoptation;
 
+import android.app.NotificationManager;
+import android.content.Context;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+        resetBadgeCounter();
+    }
+
+    private void resetBadgeCounter() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.cancelAll();
+        }
+    }
+}
