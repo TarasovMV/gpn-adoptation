bc5d92d6a8286eb37b1cf1cbe107fff36ac9a707
Mikhail Gunenkov <m.gunenkov@gmail.com>
2022-01-28 16:53:36 +0600

    Updated to Ionic 6

--- src/app/core/interceptors/cache.interceptor.ts
+++ src/app/core/interceptors/cache.interceptor.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
-import {Observable, of, throwError} from 'rxjs';
+import {from, Observable, of, throwError} from 'rxjs';
 import {catchError, map, switchMap, tap} from 'rxjs/operators';
 import {CacheService} from "../services/platform/cache.service";
-import {fromPromise} from "rxjs/internal-compatibility";
 
 @Injectable({
     providedIn: 'root'
@@ -13,10 +12,10 @@ export class CacheInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const cachingResponse: Promise<HttpResponse<any>> = this.cacheService.getCache(req.url, req.method);
-        return fromPromise(cachingResponse).pipe(
+        return from(cachingResponse).pipe(
             switchMap(x => {
                 if (!!x) {
-                    return fromPromise(cachingResponse);
+                    return from(cachingResponse);
                 } else {
                     return next.handle(req).pipe(
                         tap(res => this.cacheService.cacheResponse(req.url, req.method, res))
