f36f2f143abc331e2b0585d42b86381e81d71d80
Mikhail Gunenkov <m.gunenkov@gmail.com>
2022-08-17 20:51:50 +0600

    Updates

--- src/app/core/services/api/api-version-service.ts
+++ src/app/core/services/api/api-version-service.ts
@@ -40,7 +40,8 @@ export class ApiVersionService {
         }
         // eslint-disable-next-line no-underscore-dangle
         if (this._platform.is("ios")) {
-            this.setStoreUrl("https://apps.apple.com/ru/app/адаптация/id1573528993");
+            // eslint-disable-next-line max-len
+            this.setStoreUrl("https://apps.apple.com/us/app/%D0%BC%D0%BE%D1%8F-%D0%B0%D0%B4%D0%B0%D0%BF%D1%82%D0%B0%D1%86%D0%B8%D1%8F/id1639799856");
         }
     }
 
